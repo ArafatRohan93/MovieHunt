64db2429ddb16eb1e8441defeeaf7c4eeaaf8b4a
Arafat Rohan <arafat.rohan@vivasoftltd.com>
2026-02-17 09:24:00 +0600

    Initial Commit

---
+++ MovieHunt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MovieHunt
+//
+//  Created by Arafat Rohan Vivasoft on 17/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MovieHunt/MovieHuntApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieHuntApp.swift
+//  MovieHunt
+//
+//  Created by Arafat Rohan Vivasoft on 17/2/26.
+//
+
+import SwiftUI
+
+@main
+struct MovieHuntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
